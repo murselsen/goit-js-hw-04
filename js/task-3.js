70944d7a626764acea3ff881bb9708d58ec11122
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2025-01-10 12:16:35 +0300

    add profile object with methods to manage username and playtime

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,32 @@
- 
+console.log('~~~~~~~~~ Task 3: ~~~~~~~~~');
+
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  getInfo: () => {
+    // this doesn't work
+    // return `${this.username} has ${this.playTime} active hours!`;
+
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+  changeUsername: name => {
+    // this doesn't work
+    this.username = name;
+
+    profile.username = name;
+  },
+  updatePlayTime: time => {
+    // this doesn't work
+    this.playTime += time;
+
+    profile.playTime += time;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
